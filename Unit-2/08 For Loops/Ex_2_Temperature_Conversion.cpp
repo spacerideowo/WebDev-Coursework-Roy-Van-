44016cc5821af908ff4d13a3fb4ee6a163b968f6
Roy Van <spaceriderv@outlook.com>
2025-04-15 14:38:55 +0800

    Removed unnecessary test files

--- Unit-2/08 For Loops/Ex_2_Temperature_Conversion.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    for(double F = 20; F <= 20 + 4 * 19; F+=4){
-        cout << F << " degrees Fahrenheit = " << (5.0 / 9.0) * (F - 32.0) << " degrees Celsius.\n";
-    }
-    return 0;
-}
